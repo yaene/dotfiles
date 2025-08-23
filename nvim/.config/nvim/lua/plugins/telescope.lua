bf688b842ea6b23462a8dd0e462bf10234b24f85
Yannis Bolliger <yannis.bolliger@protonmail.com>
2025-08-23 17:43:32 +0200

    nvim: telescope: find files/live grep with hiden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,37 +1,44 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    -- todo hallo
+   "nvim-telescope/telescope.nvim",
+   branch = "0.1.x",
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-tree/nvim-web-devicons",
+      "folke/todo-comments.nvim",
+   },
+   config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      -- todo hallo
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          }
-        }
-      }
-    })
-    telescope.load_extension("fzf")
+      telescope.setup({
+         defaults = {
+            path_display = { "smart" },
+            mappings = {
+               i = {
+                  ["<C-p>"] = actions.move_selection_previous,
+                  ["<C-n>"] = actions.move_selection_next,
+                  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+               },
+            },
+         },
+      })
+      telescope.load_extension("fzf")
 
-    local keymap = vim.keymap
+      local keymap = vim.keymap
+      local builtin = require("telescope.builtin")
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>faf", function()
+         builtin.find_files({ hidden = true })
+      end, { desc = "Fuzzy find files in cwd including hidden" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fas", function()
+         builtin.live_grep({ hidden = true })
+      end, { desc = "Find string in cwd including hidden files" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+      keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+   end,
 }

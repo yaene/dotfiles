2e1a64eef0b5d02061fe2198732ca2e029c1c921
Yannis Bolliger <yannis.bolliger@protonmail.com>
2025-08-24 21:59:44 +0200

    nvim: fix find string w/ hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -36,7 +36,7 @@ return {
       keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
       keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
       keymap.set("n", "<leader>fas", function()
-         builtin.live_grep({ hidden = true })
+         builtin.live_grep({ additional_args = { "--hidden" } })
       end, { desc = "Find string in cwd including hidden files" })
       keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
       keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })

183cf784d4cbd0120a1e0da17c3a3d6f3ee9070e
Yannis Bolliger <yannis.bolliger@protonmail.com>
2025-09-17 11:56:48 +0200

    nvim: remove autopairs as included in blink

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -68,6 +68,7 @@ return { -- Autocompletion
          -- By default, you may press `<c-space>` to show the documentation.
          -- Optionally, set `auto_show = true` to show the documentation after a delay.
          documentation = { auto_show = false, auto_show_delay_ms = 500 },
+         accept = { auto_brackets = { enabled = true } },
       },
 
       sources = {

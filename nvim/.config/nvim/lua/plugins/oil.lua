070fdcb09d8464fe837e7cdfcbc2974baa30d6bc
Yannis Bolliger <yannis.bolliger@protonmail.com>
2026-08-07 11:51:28 +0200

    add oil keymaps

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,1 +1,13 @@
-require("oil").setup()
+local oil = require("oil")
+oil.setup({
+   float = {
+      border = "rounded",
+   },
+})
+
+local keymap = vim.keymap
+
+keymap.set({ "n", "v" }, "<leader>ef", "<cmd>Oil --float<cr>", { desc = "Open oil at the current file" })
+keymap.set("n", "<leader>ee", function()
+   oil.open_float(vim.fn.getcwd())
+end, { desc = "Open the file manager in nvim's working directory" })
